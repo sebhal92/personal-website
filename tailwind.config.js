45d5223cfb3bcfb778a4f49d8c4872608c0f3fe4
sebhal92 <58229006+sebhal92@users.noreply.github.com>
2026-06-17 23:32:37 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-tailwind.config.js
+module.exports = {
+  content: [
+    "./_layouts/**/*.html",
+    "./_includes/**/*.html",
+    "./*.html",
+    "./assets/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/container-queries")
+  ],
+};
